a4f23101246862026da222a818fe4894a7ea490d
rpassosdomingues <rafael.domingues@sou.unifal-mg.edu.br>
2023-09-27 11:27:05 -0300

    sep 27 wed

--- 1_cache-memory/2023-2/aeds2_pratica/2.trabalhos_praticos/trabalho 01/horse_walk/horseWalk.c
+++ 1_cache-memory/2023-2/aeds2_pratica/2.trabalhos_praticos/trabalho 01/horse_walk/horseWalk.c
@@ -7,7 +7,7 @@
  * 
  * This code was developed with careful consultation 
  * with Bing artificial intelligence.
-*/
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,8 +20,8 @@
  * It allocates memory for the new node using the
  * malloc function, sets the node coordinates, and
  * initializes the next and prev pointers to NULL.
-*/
-Horse* newHouse (int x, int y) {
+ */
+Horse* newHouse(int x, int y) {
     Horse* horse = (Horse*)malloc(sizeof(Horse));
     horse->x = x;
     horse->y = y;
@@ -39,8 +39,8 @@ Horse* newHouse (int x, int y) {
  * 
  * Otherwise, it traverses the list to the end and adds
  * the new node there.
-*/
-void addHouse (Horse** head, int x, int y) {
+ */
+void addHouse(Horse** head, int x, int y) {
     Horse* horse = newHouse(x, y);
     if (*head == NULL) {
         *head = horse;
@@ -54,13 +54,23 @@ void addHouse (Horse** head, int x, int y) {
     }
 }
 
+void addEnd(Horse* start, int x, int y) {
+    Horse* lastHouse = newHouse(x, y);
+    Horse* temp = start;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = lastHouse;
+    lastHouse->prev = temp;
+}
+
 /**
  * This function prints the coordinates of each node in the linked list.
  * 
  * It starts at the head node of the list and follows the next pointers until
  * it reaches the end of the list.
-*/
-void printList (Horse* head) {
+ */
+void printList(Horse* head) {
     Horse* temp = head;
     while (temp != NULL) {
         printf("(%d, %d)\n", temp->x, temp->y);
@@ -68,24 +78,28 @@ void printList (Horse* head) {
     }
 }
 
-void addEnd (Horse* start, int x, int y) {
-    Horse* newHouse = lastHouse(x, y);
-    Horse* temp = start;
-    while (temp->next != NULL) {
-        temp = temp->next;
-    }
-    temp->next = newHouse;
-    newHouse->prev = temp;
-}
-
 /**
  * Check whether a move is valid within the chessboard
-*/
-int validMove (int x, int y, int M, int N) {
+ */
+int validMove(int x, int y, int N, int M) {
     return (x >= 0 && x < M && y >= 0 && y < N);
 }
 
-int countTripsClosed (Horse horse, int M, int N, bool **chessboard) {
+/**
+ * Check if all squares on the chessboard have been visited
+ */
+int allHouses(int** visited, int N, int M) {
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            if (!visited[i][j]) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+int countTripsClosed(Horse horse, int N, int M, bool **chessboard) {
     int** visited = (int**)malloc(M * sizeof(int*));
     for (int i = 0; i < M; i++) {
         visited[i] = (int*)malloc(N * sizeof(int));
@@ -94,27 +108,27 @@ int countTripsClosed (Horse horse, int M, int N, bool **chessboard) {
         }
     }
 
-    Horse* list = newHouse (horse.x, horse.y);
+    Horse* list = newHouse(horse.x, horse.y);
     int countClosed = 0;
 
     while (list != NULL) {
-        int x = list->x;
-        int y = list->y;
-        visited[x][y] = 1;
-        Horse* next = list->next;
+    int x = list->x;
+    int y = list->y;
+    visited[x][y] = 1;
+    Horse* next = list->next;
 
-        for (int i = 0; i < 8; i++) {
-            int newX = x + horse.moves[i][0];
-            int newY = y + horse.moves[i][1];
-            if (validMove (newX, newY, M, N) && !visited[newX][newY] && !chessboard[newX][newY]) {
-                if (next == NULL && allHouses (visited, M, N) && newX == horse.x && newY == horse.y) {
-                    countClosed++;
-                } else {
-                    adicionarAoFim(next, newX, newY);
-                }
-                visited[newX][newY] = 0;
+    for (int i = 0; i < 8; i++) {
+        int newX = x + horse.moves[i][0];
+        int newY = y + horse.moves[i][1];
+        if (validMove(newX, newY, M, N) && !visited[newX][newY] && !chessboard[newX][newY]) {
+            if (next == NULL && allHouses(visited, M, N) && newX == horse.x && newY == horse.y) {
+                countClosed++;
+            } else {
+                addEnd(next, newX, newY);
             }
+            visited[newX][newY] = 0;
         }
+    }
         free(list);
         list = next;
     }
@@ -127,80 +141,62 @@ int countTripsClosed (Horse horse, int M, int N, bool **chessboard) {
     return countClosed;
 }
 
-/**
- * Check if all squares on the chessboard have been visited
-*/
-int allHouses (int** visited, int M, int N) {
-    for (int i = 0; i < M; i++) {
-        for (int j = 0; j < N; j++) {
-            if (!visited[i][j]) {
-                return 0;
-            }
-        }
-    }
-    return 1;
-}
-
-void calculatesTrips (bool **chessboard) {
+void calculatesTrips(bool **chessboard, int N, int M) {
     int closed = 0;
     int open = 0;
-    
-    // Chesschessboard dimensions
-    int M = 8;  
-    int N = 8;
 
+    // Possible movements of the horse.
     Horse horse = {0, 0, {{2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}, {1, -2}, {2, -1}}};
-    closed = countTripsClosed(horse, M, N, chessboard);
+    closed = countTripsClosed(horse, N, M, chessboard);
 
     printf("%d\n%d\n", closed, open);
-    return;
 }
 
-int main (int argc, char* argv[]) {
+int main(int argc, char* argv[]) {
     int instance_num = -1;
     instance_num = atoi(argv[1]);
-    if (instance_num <= 0 || instance_num > 20) {
+    if (instance_num <= 0 || instance_num > 10) {
         printf("To run the code, type ./horseWalk x\nwhere x is a number between 1 and 10 that symbolizes the instance used.\n");
         exit(0);
     }
-    
+
+    // Dimensions of the chessboard
+    int n, m;
+
     // Create the chessboard
-    bool **chessboard = readInstance(instance_num);
+    bool **chessboard = readInstance(instance_num, &n, &m);
 
-    calculatesTrips(chessboard);
+    calculatesTrips(chessboard, n, m);
 
-    return (1);
+    return 0;
 }
 
-bool **readInstance(int instance_num) {
-    int n, m;
+bool **readInstance(int instance_num, int *n, int *m) {
     int i;
-    
     // Mount the trip to instance
     char *file_instance = "./instances/";
     asprintf(&file_instance, "%s%d", file_instance, instance_num);
-    
+
     // Pointer to instance
     FILE* file;
- 
+
     // Open the instance in read mode
     file = fopen(file_instance, "r");
- 
+
     if (NULL == file) {
         printf("Instance file not found! Check your directories.\n");
         exit(0);
     }
 
     // Read the instances file
-    fscanf (file, "%d", &n);
-    fscanf (file, "%d", &m);
+    fscanf(file, "%d", n);
+    fscanf(file, "%d", m);
 
     // Allocating the chessboard dynamically
-    // Using calloc instead of malloc to initialize the entire chesschessboard with zeros
-    bool** chessboard = (bool**)calloc(n, sizeof(bool*));
-    for (int i = 0; i < n; i++) {
-        chessboard[i] = (bool*)calloc(m, sizeof(bool));
+    // Using calloc instead of malloc to initialize the entire chessboard with zeros
+    bool** chessboard = (bool**)calloc(*n, sizeof(bool*));
+    for (i = 0; i < *n; i++) {
+        chessboard[i] = (bool*)calloc(*m, sizeof(bool));
     }
-
     return chessboard;
 }
--- 1_cache-memory/2023-2/aeds2_pratica/2.trabalhos_praticos/trabalho 01/horse_walk/horseWalk.h
+++ 1_cache-memory/2023-2/aeds2_pratica/2.trabalhos_praticos/trabalho 01/horse_walk/horseWalk.h
@@ -19,11 +19,11 @@ Horse* newHouse (int x, int y);
 void addHouse (Horse** head, int x, int y);
 void printList (Horse* head);
 void addEnd (Horse* start, int x, int y);
-int validMove(int x, int y, int M, int N);
-int countTripsClosed (Horse horse, int M, int N, bool **chessboard);
-int allHouses (int** visited, int M, int N);
+int validMove(int x, int y, int N, int M);
+int countTripsClosed (Horse horse, int N, int M, bool **chessboard);
+int allHouses (int** visited, int N, int M);
 
-bool **readInstance(int instance_num);
-void calculatesTrips(bool **chessboard);
+bool **readInstance(int instance_num, int *n, int *m);
+void calculatesTrips(bool **chessboard, int N, int M);
 
 #endif // HORSEWALK_H
