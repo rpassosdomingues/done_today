ce87bbe7ee92e26bb12e6c26155dc456ebb74ff4
rpassosdomingues <rafael.domingues@sou.unifal-mg.edu.br>
2023-09-26 22:31:37 -0300

    sep 26 tue

--- 1_cache-memory/2023-2/aeds2_pratica/2.trabalhos_praticos/trabalho 01/passeioTmp.c
+++ 1_cache-memory/2023-2/aeds2_pratica/2.trabalhos_praticos/trabalho 01/passeioTmp.c
@@ -1,166 +1,52 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
 
-// Struct para representar a posição do cavalo
-typedef struct {
-    char letra;
-    int linha;
-} PosicaoCavalo;
+typedef struct Node {
+    int x, y;
+    struct Node *next, *prev;
+} Node;
 
-// Struct para representar os movimentos do cavalo
-typedef struct {
-    int dx; // Deslocamento na horizontal
-    int dy; // Deslocamento na vertical
-} MovimentoCavalo;
-
-// Struct para representar um nó da lista de posições
-typedef struct No {
-    PosicaoCavalo posicao;
-    struct No* proximo;
-    struct No* anterior;
-} NoPosicao;
-
-// Função para inicializar um nó da lista de posições
-NoPosicao* inicializarNo(PosicaoCavalo posicao) {
-    NoPosicao* no = (NoPosicao*)malloc(sizeof(NoPosicao));
-    no->posicao = posicao;
-    no->proximo = NULL;
-    no->anterior = NULL;
-    return no;
+Node* newNode(int x, int y) {
+    Node* node = (Node*)malloc(sizeof(Node));
+    node->x = x;
+    node->y = y;
+    node->next = NULL;
+    node->prev = NULL;
+    return node;
 }
 
-// Função para adicionar um nó no final da lista de posições
-void adicionarNo(NoPosicao** cabeca, PosicaoCavalo posicao) {
-    NoPosicao* novoNo = inicializarNo(posicao);
-    if (*cabeca == NULL) {
-        *cabeca = novoNo;
+void addNode(Node** head, int x, int y) {
+    Node* node = newNode(x, y);
+    if (*head == NULL) {
+        *head = node;
     } else {
-        NoPosicao* atual = *cabeca;
-        while (atual->proximo != NULL) {
-            atual = atual->proximo;
+        Node* temp = *head;
+        while (temp->next != NULL) {
+            temp = temp->next;
         }
-        atual->proximo = novoNo;
-        novoNo->anterior = atual;
+        temp->next = node;
+        node->prev = temp;
     }
 }
 
-// Função para remover e liberar a memória de um nó da lista de posições
-void removerNo(NoPosicao** cabeca, NoPosicao* no) {
-    if (*cabeca == NULL || no == NULL) {
-        return;
-    }
-    
-    if (*cabeca == no) {
-        *cabeca = no->proximo;
-    }
-
-    if (no->anterior != NULL) {
-        no->anterior->proximo = no->proximo;
-    }
-
-    if (no->proximo != NULL) {
-        no->proximo->anterior = no->anterior;
-    }
-
-    free(no);
-}
-
-void letra2numero(char letra, int *coluna) {
-    switch (letra) {
-        case 'a': *coluna = 1; break;
-        case 'b': *coluna = 2; break;
-        case 'c': *coluna = 3; break;
-        case 'd': *coluna = 4; break;
-        case 'e': *coluna = 5; break;
-        case 'f': *coluna = 6; break;
-        case 'g': *coluna = 7; break;
-        case 'h': *coluna = 8; break;
-        default: *coluna = 0; break;
+void printList(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        printf("(%d, %d)\n", temp->x, temp->y);
+        temp = temp->next;
     }
 }
 
-bool ehPar(int linha, int coluna) {
-    return (linha % 2 == 0) || (coluna % 2 == 0);
-}
+int main() {
+    Node* head = NULL;
 
-bool ehImpar(int linha, int coluna) {
-    return (linha % 2 == 1) || (coluna % 2 == 1);
-}
+    // Adicione os movimentos do cavalo à lista
+    addNode(&head, 0, 0);
+    addNode(&head, 1, 2);
+    addNode(&head, 2, 4);
+    // Continue adicionando os movimentos...
 
-void verificaCorCasa(int linha, int coluna) {
-    if (ehPar(linha, coluna)) {
-        printf("A casa eh de cor preta.\n");
-    } else {
-        printf("A casa eh de cor branca.\n");
-    }
-}
+    printList(head);
 
-void computa_passeios(bool **tabuleiro) {
-    int fechados = 0;
-    int abertos = 0;
-    int n = 8;
-    int m = 8;
-
-    // Iniciar a busca em profundidade a partir da posição inicial (0, 0)
-    NoPosicao* listaDePosicoes = NULL;
-    adicionarNo(&listaDePosicoes, (PosicaoCavalo) {0, 0});
-
-    while (listaDePosicoes != NULL) {
-        PosicaoCavalo posicaoAtual = listaDePosicoes->posicao;
-        int x = posicaoAtual.letra - 'a';
-        int y = posicaoAtual.linha - 1;
-        int movimentos = x * m + y + 1;
-
-        if (movimentos == n * m) {
-            if (x == 0 && y == 0) {
-                if (movimentos % 2 == 0) {
-                    fechados++;
-                } else {
-                    abertos++;
-                }
-            }
-        } else {
-            // Movimentos do cavalo
-            int dx[8] = {2, 1, -1, -2, -2, -1, 1, 2};
-            int dy[8] = {1, 2, 2, 1, -1, -2, -2, -1};
-
-            for (int i = 0; i < 8; i++) {
-                int newX = x + dx[i];
-                int newY = y + dy[i];
-                if (newX >= 0 && newX < n && newY >= 0 && newY < m && !tabuleiro[newX][newY]) {
-                    tabuleiro[newX][newY] = true;
-                    adicionarNo(&listaDePosicoes, (PosicaoCavalo) {'a' + newX, newY + 1});
-                }
-            }
-        }
-
-        // Remover a posição atual da lista
-        NoPosicao* proximo = listaDePosicoes->proximo;
-        removerNo(&listaDePosicoes, listaDePosicoes);
-        listaDePosicoes = proximo;
-    }
-
-    // Imprimir resultados
-    printf("Passeios Fechados: %d\nPasseios Abertos: %d\n", fechados, abertos);
-}
-
-int main(int argc, char* argv[]) {
-    ///////////////////////////////////////////////////////////
-    /////////////////// Leitor de instâncias //////////////////
-    ///////////////// Não deve ser modificado /////////////////
-    ///////////////////////////////////////////////////////////
-    int instancia_num = -1;
-    instancia_num = atoi(argv[1]);
-    if (instancia_num <= 0 || instancia_num > 20) {
-        printf("Para executar o código, digite ./passeio x\nonde x é um número entre 1 e 20 que simboliza a instância utilizada\n");
-        exit(0);
-    }
-    
-    // Tabuleiro do jogo
-    bool **tabuleiro = ler_instancia(instancia_num);
-
-    computa_passeios(tabuleiro);
-
-    return (1);
+    return 0;
 }
