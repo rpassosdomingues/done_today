1f57129d04d2e538fc4505cdb8990f8ef5ac2078
rpassosdomingues <rafael.domingues@sou.unifal-mg.edu.br>
2023-12-07 18:37:32 -0300

    dec 7 thu

--- cache/2023-2/aeds2-pratica/trabalhos-praticos/trabalho2/codigo_base/arvores.h
+++
@@ -1,9 +0,0 @@
-#ifndef _H_ARVORES
-#define _H_ARVORES
-
-// Implementar qualquer struct que for necessária aqui
-
-double arvore_binaria(int instancia_num);
-double arvore_balanceada(int instancia_num);
-
-#endif // _H_ARVORES
