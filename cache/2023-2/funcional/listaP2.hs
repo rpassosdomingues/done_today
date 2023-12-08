bfb37363025b3a3a579f33d9c355224a8d41819a
rpassosdomingues <rafael.domingues@sou.unifal-mg.edu.br>
2023-12-08 15:40:49 -0300

    dec 8 fri

--- cache/2023-2/funcional/listaP2.hs
+++ cache/2023-2/funcional/listaP2.hs
@@ -16,20 +16,16 @@ listaIntB = [100..90]
 -- Questão 1 --
 -- =============================================================================================
 
--- Função principal
 ehPerfeito :: Int -> Bool
 ehPerfeito n = (somaDivisores n) == n
 
--- Função auxiliar para obter a soma dos divisores de um número
 somaDivisores :: Int -> Int
 somaDivisores m = acumulador [divisor | divisor <- [1..m-1], (ehDivisivel m divisor)]
 
--- Função auxiliar para acumular a soma dos divisores
 acumulador :: [Int] -> Int
 acumulador [] = 0
 acumulador (x:xs) = foldr (+) x xs -- x + acumulador xs
 
--- Função auxiliar para verificar se um número é divisível por outro, evitando divisão por zero.
 ehDivisivel :: Int -> Int -> Bool
 ehDivisivel _ 0 = False
 ehDivisivel a b = (mod a b) == 0
@@ -38,21 +34,17 @@ ehDivisivel a b = (mod a b) == 0
 -- Questão 2 --
 -- =============================================================================================
 
--- Função principal
 contaString :: [Char] -> [(Char, Int)]
 contaString xs = contaStringAux xs xs
 
--- Função auxiliar recursiva
 contaStringAux :: [Char] -> [Char] -> [(Char, Int)]
 contaStringAux _ [] = []
 contaStringAux original (x:xs) =
   (x, contaChar x original) : contaStringAux original xs
 
--- Função auxiliar para somar as letras
 contaChar :: Char -> [Char] -> Int
 contaChar l xs = foldr (somaSeIgual l) 0 xs
 
--- Função auxiliar para somar se o caractere for igual
 somaSeIgual :: Char -> Char -> Int -> Int
 somaSeIgual l x acc
   | x == l = acc + 1
@@ -82,8 +74,11 @@ quadrado q = q * q
 
 simulaCartesiano :: [Int] -> [Int] -> [(Int, Int)]
 simulaCartesiano [] _ = []
-simulaCartesiano _ [] = []
-simulaCartesiano (x:xs) (y:ys) = [(x, y) | y <- ys] ++ simulaCartesiano ys xs
+simulaCartesiano (x:xs) listaB = combinaElementos x listaB ++ simulaCartesiano xs listaB
+
+combinaElementos :: Int -> [Int] -> [(Int, Int)]
+combinaElementos _ [] = []
+combinaElementos x (y:ys) = (x, y) : combinaElementos x ys
 
 -- =============================================================================================
 -- Questão 6 --
