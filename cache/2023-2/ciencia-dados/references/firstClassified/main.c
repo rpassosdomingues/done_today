14ae6c2b379b7899e781ca541f7c23438e2ffdc8
rpassosdomingues <rafael.domingues@sou.unifal-mg.edu.br>
2023-12-23 09:41:38 -0300

    dec 23 sat

---
+++ cache/2023-2/ciencia-dados/references/firstClassified/main.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// Função de ativação sigmoide
+double sigmoid(double x) {
+    return 1.0 / (1.0 + exp(-x));
+}
+
+// Função de perda de entropia cruzada binária
+double binary_cross_entropy(double y_true, double y_pred) {
+    return -((y_true * log(y_pred)) + ((1 - y_true) * log(1 - y_pred)));
+}
+
+// Estrutura do modelo do classificador binário
+typedef struct {
+    double weight;
+    double bias;
+} BinaryClassifier;
+
+// Inicializar o classificador binário
+void initialize_classifier(BinaryClassifier* model) {
+    model->weight = ((double)rand() / RAND_MAX) * 2.0 - 1.0;  // Inicialização aleatória entre -1 e 1
+    model->bias = 0.0;
+}
+
+// Realizar o forward pass
+double forward_pass(BinaryClassifier* model, double x) {
+    return sigmoid(model->weight * x + model->bias);
+}
+
+// Treinar o classificador binário usando gradiente descendente
+void train_classifier(BinaryClassifier* model, double x, double y_true, double learning_rate, int epochs) {
+    for (int epoch = 0; epoch < epochs; ++epoch) {
+        // Forward pass
+        double y_pred = forward_pass(model, x);
+
+        // Calcular a perda
+        double loss = binary_cross_entropy(y_true, y_pred);
+
+        // Calcular gradientes
+        double gradient_w = (y_pred - y_true) * x;
+        double gradient_b = y_pred - y_true;
+
+        // Atualizar parâmetros usando gradiente descendente
+        model->weight -= learning_rate * gradient_w;
+        model->bias -= learning_rate * gradient_b;
+
+        // Exibir a perda a cada 100 épocas
+        if (epoch % 100 == 0) {
+            printf("Época %d, Perda: %.4f\n", epoch, loss);
+        }
+    }
+}
+
+int main() {
+    // Configurar uma semente aleatória para reprodução
+    srand(42);
+
+    // Criar um modelo de classificador binário
+    BinaryClassifier model;
+    initialize_classifier(&model);
+
+    // Dados de treinamento de exemplo
+    double x_train = 1.0;
+    double y_true_train = 0.0;  // Rótulo esperado (0 para exemplo de negativo)
+
+    // Hiperparâmetros
+    double learning_rate = 0.01;
+    int epochs = 1000;
+
+    // Treinar o modelo
+    train_classifier(&model, x_train, y_true_train, learning_rate, epochs);
+
+    // Testar o modelo treinado
+    double y_pred = forward_pass(&model, x_train);
+    printf("Resultado do Teste: %.4f\n", y_pred);
+
+    return 0;
+}
