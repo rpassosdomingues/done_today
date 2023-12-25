aef1d4bb9a1225183405088eb7f1ce333b932b92
rpassosdomingues <rafael.domingues@sou.unifal-mg.edu.br>
2023-12-25 11:27:55 -0300

    dec 25 mon

--- cache/2023-2/ciencia-dados/references/firstClassified/source/epochLoss.py
+++ cache/2023-2/ciencia-dados/references/firstClassified/source/epochLoss.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('../data/epochs_losses.csv')
 
-plt.plot(df['Epoch'], df['Loss'])
+plt.scatter(df['Epoch'], df['Loss'])
 plt.title('Loss Over Epochs')
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
