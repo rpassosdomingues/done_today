2c99f6ad29b2764483e35b6d72c858035812c77d
rpassosdomingues <rafael.domingues@sou.unifal-mg.edu.br>
2023-12-30 08:54:46 -0300

    dec 30 sat

--- cache/2023-2/ciencia-dados/tests/svm/main.c
+++ cache/2023-2/ciencia-dados/tests/svm/main.c
@@ -1,14 +1,20 @@
 /**
- * ____________________________________________
- * --- SVM apply regression in time series ---
- * --------------------------------------------
+ * ____________________________________________________________
+ *             --- Time series forecasting ---
+ * ------------------------------------------------------------
+ * using a combination of methods including feature extraction,
+ * training a support vector machine (SVM) model,
+ * and forecasting with a sliding window
+ * ------------------------------------------------------------
  * Author: Rafael Passos Domingues
- * ____________________________________________
+ * ____________________________________________________________
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#include <fftw3.h>
 #include <libsvm/svm.h>
 
 #include "main.h"
@@ -61,20 +67,70 @@ void readData(const char *filename, TimeSeries **data, int *numEntries) {
     fclose(file);
 }
 
-// Function to extract features (von Neumann entropy and FFT)
-void extractFeatures(const TimeSeries *data, int numEntries, double *vonNeumannEntropy, double *fftResult) {
-    // Implement von Neumann entropy calculation and FFT here
-    // Replace the following lines with your implementation
+// Function to calculate von Neumann entropy
+double vonNeumannEntropy(const double *data, int size) {
+    double sum = 0.0;
 
-    // Dummy values for demonstration purposes
+    for (int i = 0; i < size; i++) {
+        double probability = data[i] / 100.0;  // Assuming data is in the range [0, 100]
+        if (probability > 0.0) {
+            sum += probability * log2(probability);
+        }
+    }
+
+    return -sum;
+}
+
+// Function to perform FFT using FFTW library
+void performFFT(const double *data, int size, double *result) {
+    fftw_complex *in, *out;
+    fftw_plan plan;
+
+    in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * size);
+    out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * size);
+
+    for (int i = 0; i < size; i++) {
+        in[i][0] = data[i];
+        in[i][1] = 0.0;
+    }
+
+    plan = fftw_plan_dft_1d(size, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+    fftw_execute(plan);
+
+    for (int i = 0; i < size; i++) {
+        result[i] = out[i][0];
+    }
+
+    fftw_destroy_plan(plan);
+    fftw_free(in);
+    fftw_free(out);
+}
+
+// Function to extract features (von Neumann entropy and FFT)
+void extractFeatures(const TimeSeries *data, int numEntries, double *vonNeumannEntropyArray, double *fftResult) {
     for (int i = 0; i < numEntries; i++) {
-        vonNeumannEntropy[i] = i * 0.1;
-        fftResult[i] = i * 0.2;
+        // Assuming vonNeumannEntropyArray, fftResult are arrays with appropriate sizes
+        double vonNeumannData[numEntries]; // Use numEntries as the size
+        double fftData[numEntries];        // Use numEntries as the size
+
+        // Populate vonNeumannData and fftData arrays (replace with your logic)
+        vonNeumannData[i] = data[i].price * 0.1; // Replace with actual calculation
+        fftData[i] = data[i].price * 0.2;        // Replace with actual calculation
+
+        // Calculate von Neumann entropy
+        vonNeumannEntropyArray[i] = vonNeumannEntropy(vonNeumannData, numEntries);
+
+        // Perform FFT
+        performFFT(fftData, numEntries, fftResult);
+
+        // Calculate subjective features (replace with your logic)
+        data[i].subjectiveFeature1 = data[i].price * 0.3;
+        data[i].subjectiveFeature2 = data[i].price * 0.4;
     }
 }
 
-// Function to train and evaluate SVM model
-void trainAndEvaluateSVM(const TimeSeries *data, int numEntries) {
+// Function to train SVM model
+struct svm_model* trainSVM(const TimeSeries *data, int numEntries) {
     // Extract features
     double vonNeumannEntropy[numEntries];
     double fftResult[numEntries];
@@ -97,7 +153,6 @@ void trainAndEvaluateSVM(const TimeSeries *data, int numEntries) {
 
     // Set SVM parameters
     struct svm_parameter param;
-    //svm_parameter_init(&param);
     param.svm_type = EPSILON_SVR;
     param.kernel_type = RBF;
     param.eps = 0.001;
@@ -116,22 +171,135 @@ void trainAndEvaluateSVM(const TimeSeries *data, int numEntries) {
     }
     free(prob.x);
     free(prob.y);
-    svm_free_and_destroy_model(&model);
+
+    return model;
+}
+
+// Function to train and evaluate SVM model
+void trainAndEvaluateSVM(const TimeSeries *data, int numEntries, struct svm_model **model) {
+    // Extract features
+    double vonNeumannEntropy[numEntries];
+    double fftResult[numEntries];
+    extractFeatures(data, numEntries, vonNeumannEntropy, fftResult);
+
+    // Prepare data for LIBSVM
+    struct svm_problem prob;
+    prob.l = numEntries;
+    prob.y = (double *)malloc(numEntries * sizeof(double));
+    prob.x = (struct svm_node **)malloc(numEntries * sizeof(struct svm_node *));
+    for (int i = 0; i < numEntries; i++) {
+        prob.x[i] = (struct svm_node *)malloc(3 * sizeof(struct svm_node));
+        prob.x[i][0].index = 1;
+        prob.x[i][0].value = vonNeumannEntropy[i];
+        prob.x[i][1].index = 2;
+        prob.x[i][1].value = fftResult[i];
+        prob.x[i][2].index = -1; // Indicates the end of data
+        prob.y[i] = data[i].price;
+    }
+
+    // Set SVM parameters
+    struct svm_parameter param;
+    param.svm_type = EPSILON_SVR;
+    param.kernel_type = RBF;
+    param.eps = 0.001;
+    param.C = 1.0;
+
+    // Train SVM model
+    *model = svm_train(&prob, &param);
+
+    // Evaluate SVM model (dummy prediction for demonstration purposes)
+    double predictValue = svm_predict(*model, prob.x[0]);
+    printf("SVM Prediction: %f\n", predictValue);
+
+    // Free allocated memory
+    for (int i = 0; i < numEntries; i++) {
+        free(prob.x[i]);
+    }
+    free(prob.x);
+    free(prob.y);
+}
+
+Forecast* forecast(const TimeSeries *data, int numEntries, int windowSize, int numSteps, const struct svm_model *model) {
+    // Extract features with the sliding window
+    double vonNeumannEntropy[numEntries];
+    double fftResult[numEntries];
+    extractFeatures(data, numEntries, vonNeumannEntropy, fftResult);
+
+    // Prepare data for LIBSVM
+    Forecast *forecastData = (Forecast *)malloc(numSteps * sizeof(Forecast));
+    if (forecastData == NULL) {
+        perror("\n\tErro na alocação de memória.\n\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < numSteps; i++) {
+        // Extract features for the current step using the last window
+        double currentVonNeumannEntropy, currentFFTResult;
+
+        currentVonNeumannEntropy = vonNeumannEntropy[numEntries - windowSize + i];
+        currentFFTResult = fftResult[numEntries - windowSize + i];
+
+        // Prepare input vector for prediction
+        struct svm_node x[3];
+        x[0].index = 1;
+        x[0].value = currentVonNeumannEntropy;
+        x[1].index = 2;
+        x[1].value = currentFFTResult;
+        x[2].index = -1; // Indicates the end of the data
+
+        // Predict using the SVM model
+        double prediction = svm_predict(model, x);
+
+        // Store the predicted value along with the date
+        strcpy(forecastData[i].date, data[numEntries - windowSize + i].date);
+        forecastData[i].predictedPrice = prediction;
+    }
+
+    return forecastData;
 }
 
 int main() {
-    
     const char *filename = "../data/input.csv";
     TimeSeries *data;
     int numEntries;
+    int windowSize = 10; // Fit an appropriate window size // to-do
+    int numSteps = 100;
 
     readData(filename, &data, &numEntries);
 
-    // Train and evaluate SVM model
-    trainAndEvaluateSVM(data, numEntries);
+    // Train SVM model
+    struct svm_model *model = trainSVM(data, numEntries);
+    trainAndEvaluateSVM(data, numEntries, &model);
+
+    // Extract features for forecast
+    double vonNeumannEntropy[numEntries];
+    double fftResult[numEntries];
+
+    extractFeatures(data, numEntries, vonNeumannEntropy, fftResult);
+
+    // Forecast with Sliding Window
+    Forecast *forecastData = forecast(data, numEntries, windowSize, numSteps, model);
+
+    // Forecast values and write to output file
+    FILE *outputFile = fopen("../data/output.csv", "w");
+    if (outputFile == NULL) {
+        perror("\n\tError opening output file.\n\n");
+        exit(EXIT_FAILURE);
+    }
+
+    fprintf(outputFile, "Date,PredictedPrice,SubjectiveFeature1,SubjectiveFeature2\n");
+    for (int i = 0; i < numSteps; i++) {
+        fprintf(outputFile, "%s,%f,%f,%f\n", forecastData[i].date, forecastData[i].predictedPrice,
+                data[numEntries - windowSize + i].subjectiveFeature1, data[numEntries - windowSize + i].subjectiveFeature2);
+    }
+
+    // Close the output file
+    fclose(outputFile);
 
     // Free allocated memory
     free(data);
+    free(forecastData);
+    svm_free_and_destroy_model(&model);
 
     return 0;
 }
--- cache/2023-2/ciencia-dados/tests/svm/main.h
+++ cache/2023-2/ciencia-dados/tests/svm/main.h
@@ -4,11 +4,24 @@
 typedef struct {
     char date[20];
     double price;
+    double subjectiveFeature1;
+    double subjectiveFeature2;
+    // Add more subjective features as needed
 } TimeSeries;
+
+typedef struct {
+    char date[20];
+    double predictedPrice;
+} Forecast;
 
 void readData(const char *filename, TimeSeries **data, int *numEntries);
 
-void extractFeatures(const TimeSeries *data, int numEntries, double *vonNeumannEntropy, double *fftResult);
-void trainAndEvaluateSVM(const TimeSeries *data, int numEntries);
+double vonNeumannEntropy(const double *data, int size);
 
-#endif // _H_TIMESERIES_
+void performFFT(const double *data, int size, double *result);
+void extractFeatures(const TimeSeries *data, int numEntries, double *vonNeumannEntropyArray, double *fftResult);
+void trainAndEvaluateSVM(const TimeSeries *data, int numEntries, struct svm_model **model);
+
+Forecast* forecast(const TimeSeries *data, int numEntries, int windowSize, int numSteps, const struct svm_model *model);
+
+#endif //_H_TIMESERIES_
